3564bb3960e3610b996c204d0d1b9c93e5096bb5
Ruslan Chernov <chernruslan@gmail.com>
2017-02-22 13:24:48 +0700

    'Add forach'

--- white_wine.php
+++ white_wine.php
@@ -1,12 +1,29 @@
 <?php
-    $re = '/<p\sclass="grid-link__title">\s*([\s\S]*?)\s*<\/p>[\n\t\s]*<p\sclass="grid-link__meta">\s*(£?.*)[\n\s]*<\/p>/';
-$str = file_get_contents('https://shop.hopburnsblack.co.uk/collections/white-wine');
+  $re = '/<p\sclass="grid-link__title">\s*([\s\S]*?)\s*<\/p>[\n\t\s]*<p\sclass="grid-link__meta">\s*(£?.*)[\n\s]*<\/p>/';
 
-preg_match_all($re, $str, $matches);
+  $opt = array(
+    'http' => array(
+      'proxy' => 'tcp://proxy:6666',
+      'request_fulluri' => true,
+    ),
+  );
 
-$fp = fopen('file.csv', 'w');
+  $context = stream_context_create($opt);
+  $str = file_get_contents('https://shop.hopburnsblack.co.uk/collections/white-wine', false, $context);
 
-for($i = 0, $n = count($matches[1]); $i < $n; $i++) {
-  fputcsv($fp, array($matches[1][$i],$matches[2][$i]));
-}
+  preg_match_all($re, $str, $matches);
+  array_shift($matches);
+
+  $table1 = fopen('file1.csv', 'w');
+  $table2 = fopen('file2.csv', 'w');
+
+  for($i = 0, $n = count($matches[0]); $i < $n; $i++) {
+    fputcsv($table1, array($matches[0][$i], $matches[1][$i]));
+  }
+
+  $arr = array_combine($matches[0], $matches[1]);
+
+  foreach ($arr as $key=>$value) {
+      fputcsv($table2, array($key,$value));
+  }
 ?>
